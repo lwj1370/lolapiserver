9b1d42054bd02a53686ab2fa465c884f39a57a6f
이우진 <lwj1370@naver.com>
2022-12-13 17:04:39 +0900

    rotten tomato 크롤링 로직 수정

--- apiserver/apiserver/rottentomatoapi/views.py
+++ apiserver/apiserver/rottentomatoapi/views.py
@@ -4,28 +4,76 @@ from rest_framework.response import Response
 import requests
 from bs4 import BeautifulSoup 
 
+def seasonInfo(link):   
+    # link = 'https://www.rottentomatoes.com/tv/wednesday'
+    response = requests.get(link)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    seasonSection = soup.find('section', {'id':'seasonList'})
+    seasonItems = seasonSection.find_all('a')
+
+    seasons = []
+    
+    for item in seasonItems:
+        season = {}
+        seassonItem = item.find('season-list-item')
+        if seassonItem != None:
+            season['href'] = 'https://www.rottentomatoes.com' + item.get('href')
+
+            info = {}
+            infos = seassonItem.get('info').split(', ')
+            info['year'] = infos[0]
+            info['ott'] = infos[1]
+            info['episodes'] = infos[2].split(' ')[0]
+            
+            season['info'] = info
+            season['posterurl'] = seassonItem.get('posterurl')
+            season['tomatometerscore'] = seassonItem.get('tomatometerscore')
+            season['tomatometerstate'] = seassonItem.get('tomatometerstate')
+
+            seasons.append(season)
+
+    return seasons
+        
+
 class NetflixListViewSet(APIView):
+
     def post(self, request, *args, **kwargs):
         url = 'https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:netflix~audience:upright~critics:fresh~sort:popular?page=1'
         response = requests.get(url)
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
         list_items = soup.find_all('span', 'p--small')
-        
+
+        result = {}
+        result['programs'] = []
+
         for item in list_items:
             if item.parent.parent.parent.parent.parent.parent.name == 'div':
+                dicItem = {}
+
                 # score-pairs 태그 정보 item.previous_sibling.previous_sibling
-                audiencescore = item.previous_sibling.previous_sibling.get('audiencescore')
-                audiencesentiment = item.previous_sibling.previous_sibling.get('audiencesentiment')
-                criticsscore = item.previous_sibling.previous_sibling.get('criticsscore')
-                criticssentiment = item.previous_sibling.previous_sibling.get('criticssentiment')
+                dicItem['link'] = 'https://www.rottentomatoes.com' + item.parent.parent.parent.get('href')
+                dicItem['title'] = item.get_text().strip()
+                dicItem['audiencescore'] = item.previous_sibling.previous_sibling.get('audiencescore')
+                dicItem['audiencesentiment'] = item.previous_sibling.previous_sibling.get('audiencesentiment')
+                dicItem['criticsscore'] = item.previous_sibling.previous_sibling.get('criticsscore')
+                dicItem['criticssentiment'] = item.previous_sibling.previous_sibling.get('criticssentiment')
+
                 posterSrc = item.parent.previous_sibling.previous_sibling.get('src')
-                lastUpdate = item.next_sibling == None if '' else item.next_sibling.get_text().strip()
-                
-                print(f'title - {item.get_text().strip()}')
-                print(f'last update - {lastUpdate}')
-                print(f'audiencescore : {audiencescore}, audiencesentiment : {audiencesentiment}, criticsscore : {criticsscore}, criticssentiment : {criticssentiment}')
-                print(f'img - {posterSrc}')
-                print('-'*20)
+                if posterSrc == None:
+                    posterSrc = item.parent.previous_sibling.previous_sibling.previous_sibling.previous_sibling.get('src')
+
+                dicItem['posterSrc'] = posterSrc
+                dicItem['lastUpdate'] = item.next_sibling == None if '' else item.next_sibling.get_text().strip()
+
+                dicItem['seasons'] = seasonInfo(dicItem['link'])
+
+                result['programs'].append(dicItem)
+
+                # print(f'title - {dicItem}')
+                # print('-' * 20)
         
-        return Response('Success')
+        return Response(result)
+
+    
